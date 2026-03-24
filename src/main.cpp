bf6bdc529c07681740830d291b87c92b8334cc5d
Lineath <fran.montserrat.developer@gmail.com>
2026-03-24 20:30:39 +0100

    initial commit, compile test

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+	//TODO remove test code
+	sf::RenderWindow window(sf::VideoMode(800, 600), "FranKanoid");
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear(sf::Color::Black);
+		window.display();
+	}
+	return (0);
+}
