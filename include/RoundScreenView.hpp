62e39d322965a4bffe1723e0fbf1be94f9a8f97e
Lineath <fran.montserrat.developer@gmail.com>
2026-03-26 14:35:23 +0100

    added wall, ceil sprites, credits and score counters

--- include/RoundScreenView.hpp
+++ include/RoundScreenView.hpp
@@ -8,9 +8,19 @@ class RoundScreenView : public ScreenView {
 
 	private:
 		std::shared_ptr<sf::Font> font;
-		std::shared_ptr<sf::Text> stubTxt;
+		std::shared_ptr<sf::Text> creditsTxt;
+		std::shared_ptr<sf::Text> scoreTxt;
 		std::shared_ptr<sf::Texture> backgroundTex;
 		std::shared_ptr<sf::Sprite> background;
 
+		std::shared_ptr<sf::Texture> wallTex;
+		std::shared_ptr<sf::Sprite> wallLeft;
+		std::shared_ptr<sf::Sprite> wallRight;
+
+		std::shared_ptr<sf::Texture> ceilTex;
+		std::shared_ptr<sf::Sprite> ceil;
+
+
 		void UpdateCredits(Game& game);
+		void UpdateScore(Game &game);
 };
--- src/RoundScreenView.cpp
+++ src/RoundScreenView.cpp
@@ -1,29 +1,67 @@
 #include "RoundScreenView.hpp"
 #include "Game.hpp"
+#include <iostream>
 
 //TODO remove 
-constexpr std::string_view STUB_STR = "STUB TEXT FOR ROUND SCREEN";
+constexpr std::string_view CREDITS_STR = "CREDITS ";
+constexpr std::string_view SCORE_STR = "SCORE ";
 
 //TODO hardcoded values
 RoundScreenView::RoundScreenView() : ScreenView() {
 	//TODO filesystem exception control
 	font = std::make_shared<sf::Font>("assets/ClearSans-Regular.ttf");
 
-	stubTxt = std::make_shared<sf::Text>(*font);
-	stubTxt->setString(std::string(STUB_STR));
-	stubTxt->setCharacterSize(24);
-	stubTxt->setFillColor(sf::Color::Blue);
-	stubTxt->setPosition({100.f, 100.f});
-	stubTxt->setOutlineColor(sf::Color::Yellow);
-	stubTxt->setOutlineThickness(4);
+	creditsTxt = std::make_shared<sf::Text>(*font);
+	creditsTxt->setString(std::string(CREDITS_STR));
+	creditsTxt->setCharacterSize(24);
+	creditsTxt->setFillColor(sf::Color::White);
+	creditsTxt->setPosition({128.f, 864.f});
+	creditsTxt->setOutlineColor(sf::Color::Blue);
+	creditsTxt->setOutlineThickness(1);
 
+	scoreTxt = std::make_shared<sf::Text>(*font);
+	scoreTxt->setString(std::string(SCORE_STR));
+	scoreTxt->setCharacterSize(24);
+	scoreTxt->setFillColor(sf::Color::White);
+	scoreTxt->setPosition({352.f, 864.f});
+	scoreTxt->setOutlineColor(sf::Color::Blue);
+	scoreTxt->setOutlineThickness(1);
+	
 	backgroundTex = std::make_shared<sf::Texture>(sf::Texture("assets/Background.png"));
 	background = std::make_shared<sf::Sprite>(sf::Sprite(*backgroundTex));
-	background->setColor(sf::Color({0,0,255,128})); //TODO tint
+	background->setColor(sf::Color({0, 0, 255, 128})); //TODO tint
+	
+	wallTex = std::make_shared<sf::Texture>(sf::Texture("assets/Wall32.png"));
+	wallTex->setRepeated(true);
+	wallLeft = std::make_shared<sf::Sprite>(sf::Sprite(*wallTex));
+	wallLeft->setTextureRect({{0, 0},{32, 896}});
+	wallLeft->setPosition({0, 32});
+	wallRight = std::make_shared<sf::Sprite>(sf::Sprite(*wallTex));
+	wallRight->setTextureRect({{0, 0},{32, 896}});
+	wallRight->setPosition({608, 32});
+
+	ceilTex = std::make_shared<sf::Texture>(sf::Texture("assets/Ceil32.png"));
+	ceilTex->setRepeated(true);
+	ceil = std::make_shared<sf::Sprite>(*ceilTex);
+	ceil->setTextureRect({{0, 0},{640, 32}});
 
 	mDrawables.push_back(background);
-	mDrawables.push_back(stubTxt);
+	mDrawables.push_back(creditsTxt);
+	mDrawables.push_back(scoreTxt);
+	mDrawables.push_back(wallLeft);
+	mDrawables.push_back(wallRight);
+	mDrawables.push_back(ceil);
 }
+
+
+/* 
+texture.setRepeated(true); // ¡Importante!
+sf::Sprite sprite(texture);
+sprite.setTextureRect({0, 0, 32, 128}); // Ancho 32, Alto 128 (repetirá el patrón)
+
+
+
+*/
 
 void RoundScreenView::ProcessInput(Game& game) {
 	auto& frameInput = game.GetInputManager().FetchInput();
@@ -34,18 +72,23 @@ void RoundScreenView::ProcessInput(Game& game) {
 		window.close();
 	if (frameInput.coin) {
 		game.AddCredit();
-		UpdateCredits(game);
-		//TODO credits gui
-/* 		if (game.GetCredits() > 0)
-			startTxt->setScale({1,1});
-		creditsTxt->setString(creditsStr); */
 	}
 	if (frameInput.action && game.GetCredits() > 0)
 		game.SetState(Game::State::ROUND_SCREEN);
+
+	UpdateCredits(game);
+	UpdateScore(game);
 }
 
 void RoundScreenView::UpdateCredits(Game& game) {
-	//TODO
-/* 	creditsStr = std::string(CREDITS_STR).append(
-		std::to_string(static_cast<int>(game.GetCredits()))); */
+	creditsTxt->setString(std::string(CREDITS_STR)
+		.append(std::to_string(static_cast<int>(game.GetCredits()))));
+}
+
+void RoundScreenView::UpdateScore(Game& game) {
+	//TODO delete score test
+	game.AddScore(1);
+
+	scoreTxt->setString(std::string(SCORE_STR)
+		.append(std::to_string(static_cast<int>(game.GetScore()))));
 }
