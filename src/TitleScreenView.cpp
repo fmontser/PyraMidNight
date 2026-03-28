2c5f73a5bb37c9e13f1008355ac8c488bb4adb7e
Lineath <fran.montserrat.developer@gmail.com>
2026-03-28 10:35:26 +0100

    name convention fixing

--- src/TitleScreenView.cpp
+++ src/TitleScreenView.cpp
@@ -1,46 +1,47 @@
 #include "TitleScreenView.hpp"
 #include "Game.hpp"
 
+constexpr std::string_view TITLE_STR = "FranKanoid";
 constexpr std::string_view CREDITS_STR = "Insert coin! credits ";
 constexpr std::string_view START_STR = "Press SPACE to start!";
 
 //TODO hardcoded values
 TitleScreenView::TitleScreenView() : ScreenView() {
 	//TODO filesystem exception control
-	font = std::make_shared<sf::Font>("assets/ClearSans-Regular.ttf");
+	mFont = std::make_shared<sf::Font>("assets/ClearSans-Regular.ttf");
 
-	titleTxt = std::make_shared<sf::Text>(*font);
-	titleTxt->setString("FranKanoid");
-	titleTxt->setCharacterSize(90);
-	titleTxt->setFillColor(sf::Color::Blue);
-	titleTxt->setPosition({100.f, 100.f});
-	titleTxt->setOutlineColor(sf::Color::Yellow);
-	titleTxt->setOutlineThickness(4);
+	mTitleTxt = std::make_shared<sf::Text>(*mFont);
+	mTitleTxt->setString(std::string(TITLE_STR));
+	mTitleTxt->setCharacterSize(90);
+	mTitleTxt->setFillColor(sf::Color::Blue);
+	mTitleTxt->setPosition({100.f, 100.f});
+	mTitleTxt->setOutlineColor(sf::Color::Yellow);
+	mTitleTxt->setOutlineThickness(4);
 
-	creditsTxt = std::make_shared<sf::Text>(*font);
-	creditsTxt->setString(std::string(CREDITS_STR).append("0"));
-	creditsTxt->setCharacterSize(48);
-	creditsTxt->setFillColor(sf::Color::Blue);
-	creditsTxt->setPosition({100.f, 500.f});
-	creditsTxt->setOutlineColor(sf::Color::Yellow);
-	creditsTxt->setOutlineThickness(3);
+	mCreditsTxt = std::make_shared<sf::Text>(*mFont);
+	mCreditsTxt->setString(std::string(CREDITS_STR).append("0"));
+	mCreditsTxt->setCharacterSize(48);
+	mCreditsTxt->setFillColor(sf::Color::Blue);
+	mCreditsTxt->setPosition({100.f, 500.f});
+	mCreditsTxt->setOutlineColor(sf::Color::Yellow);
+	mCreditsTxt->setOutlineThickness(3);
 
-	startTxt = std::make_shared<sf::Text>(*font);
-	startTxt->setScale({0,0}); //TODO Invisible workarround
-	startTxt->setString(std::string(START_STR));
-	startTxt->setCharacterSize(40);
-	startTxt->setFillColor(sf::Color::Blue);
-	startTxt->setPosition({130.f, 600.f});
-	startTxt->setOutlineColor(sf::Color::Yellow);
-	startTxt->setOutlineThickness(3);
+	mStartTxt = std::make_shared<sf::Text>(*mFont);
+	mStartTxt->setScale({0,0}); //TODO Invisible workarround
+	mStartTxt->setString(std::string(START_STR));
+	mStartTxt->setCharacterSize(40);
+	mStartTxt->setFillColor(sf::Color::Blue);
+	mStartTxt->setPosition({130.f, 600.f});
+	mStartTxt->setOutlineColor(sf::Color::Yellow);
+	mStartTxt->setOutlineThickness(3);
 
 	mBackgroundTex = std::make_shared<sf::Texture>(sf::Texture("assets/Background.png"));
 	mBackground = std::make_shared<sf::Sprite>(sf::Sprite(*mBackgroundTex));
 
 	mDrawables.push_back(mBackground);
-	mDrawables.push_back(titleTxt);
-	mDrawables.push_back(creditsTxt);
-	mDrawables.push_back(startTxt);
+	mDrawables.push_back(mTitleTxt);
+	mDrawables.push_back(mCreditsTxt);
+	mDrawables.push_back(mStartTxt);
 }
 
 void TitleScreenView::Update(Game& game) {
@@ -54,14 +55,14 @@ void TitleScreenView::Update(Game& game) {
 		game.AddCredit();
 		UpdateCredits(game);
 		if (game.GetCredits() > 0)
-			startTxt->setScale({1,1});
-		creditsTxt->setString(creditsStr);
+			mStartTxt->setScale({1,1});
+		mCreditsTxt->setString(mCreditsStr);
 	}
 	if (frameInput.action && game.GetCredits() > 0)
 		game.SetState(Game::State::ROUND_SCREEN);
 }
 
 void TitleScreenView::UpdateCredits(Game& game) {
-	creditsStr = std::string(CREDITS_STR).append(
+	mCreditsStr = std::string(CREDITS_STR).append(
 		std::to_string(static_cast<int>(game.GetCredits())));
 }
