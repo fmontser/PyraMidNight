d302fad7be46f154a5d62615d177ff68462955cf
Lineath <fran.montserrat.developer@gmail.com>
2026-03-29 18:19:06 +0200

    cursor class decoupling, added to namespace

--- include/Cursor.hpp
+++ include/Cursor.hpp
@@ -1,23 +1,28 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Common.hpp"
 
-class Cursor : public sf::RectangleShape {
-	public:
-		enum class Input {
-			LEFT, RIGHT, UP, DOWN, ACTION
-		};
+namespace fknd {
+	
+	class Cursor : public sf::RectangleShape {
+		public:
+			enum class Input {
+				LEFT, RIGHT, UP, DOWN, ACTION
+			};
+	
+			Cursor(sf::Font& font);
+	
+			void Update(Input action);
+			void SetEntry(std::shared_ptr<sf::Text>& entry);
+			bool Accept(std::string* gameEntryName);
+	
+		private:
+			std::shared_ptr<sf::Text> mEntryTxt;
+			sf::Font& mFont;
+			size_t mEntryIndex;
+			
+			void ChangeChar(Input action);
+			char SelectChar(Input action);
+	};
 
-		Cursor(sf::Font& font);
-
-		void Update(Input action);
-		void SetEntry(std::shared_ptr<sf::Text>& entry);
-		bool Accept(std::string* gameEntryName);
-
-	private:
-		std::shared_ptr<sf::Text> mEntryTxt;
-		sf::Font& mFont;
-		size_t mEntryIndex;
-		
-		void ChangeChar(Input action);
-		char SelectChar(Input action);
-};
+}
--- src/Cursor.cpp
+++ src/Cursor.cpp
@@ -1,85 +1,89 @@
 #include <algorithm>
 #include "Cursor.hpp"
 
-constexpr std::string_view CHARSET_STR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+namespace fknd {
 
-Cursor::Cursor(sf::Font& font) : mFont(font) {
-	mEntryIndex = 0;
-	auto cursorSize =  sf::Vector2f({});
-	cursorSize.x =	mFont.getGlyph('?', 48.0f, false).bounds.size.x;
-	cursorSize.y =	mFont.getGlyph('?', 48.0f, false).bounds.size.y;
-
-	setSize(cursorSize);
-	setFillColor({0, 0, 0, 0});
-	setOutlineColor({0, 255, 0, 255});
-	setOutlineThickness(2);
-}
-
-//TODO hardcoded data...
-void Cursor::Update(Input action) {
-	switch (action)
-	{
-		case Input::LEFT:
-			if (mEntryIndex > 0) {
-				move({-30,0});
-				mEntryIndex = std::clamp(--mEntryIndex, (size_t)0, (size_t)2);
-			}
-			break;
-		case Input::RIGHT:
-
-			if (mEntryIndex < 2) {
-				move({30,0});
-				mEntryIndex = std::clamp(++mEntryIndex, (size_t)0, (size_t)2);
-			}
-			break;
-		case Input::UP:
-			ChangeChar(Input::UP);
-			break;
-		case Input::DOWN:
-			ChangeChar(Input::DOWN);
-			break;
-		default:
-			break;
-	}
-}
-
-void Cursor::SetEntry(std::shared_ptr<sf::Text> &entry) { 
-	mEntryTxt = entry;
-	move(mEntryTxt->getGlobalBounds().position);
-}
-
-//TODO refactor ranking system
-bool Cursor::Accept(std::string* gameEntryName) {
-	if (gameEntryName != nullptr)
-		*gameEntryName = mEntryTxt->getString();
-	setOutlineColor({0, 0, 0, 0});
-	return true;
-}
-
-void Cursor::ChangeChar(Input action) {
-	char selected = SelectChar(action);
-	std::string str = mEntryTxt->getString();
+	constexpr std::string_view CHARSET_STR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	
-	str[mEntryIndex] = selected;
-	mEntryTxt->setString(str);
-}
-
-char Cursor::SelectChar(Input action) {
-	static int index = -1;
-	char selected = 'A';
-	const int max = CHARSET_STR.size();
-
-	switch (action)	{
-		case Input::UP:
-			index =  (index + 1) % max;
-			selected = CHARSET_STR[index];
-			break;
-		case Input::DOWN:
-			index = (index - 1 + max) % max;
-			selected = CHARSET_STR[index];
-			break;
-		default:
-			break;
+	Cursor::Cursor(sf::Font& font) : mFont(font) {
+		mEntryIndex = 0;
+		auto cursorSize =  sf::Vector2f({});
+		cursorSize.x =	mFont.getGlyph('?', CUR_FONT_SZ, false).bounds.size.x;
+		cursorSize.y =	mFont.getGlyph('?', CUR_FONT_SZ, false).bounds.size.y;
+	
+		setSize(cursorSize);
+		setFillColor(CUR_FILL_COL);
+		setOutlineColor(CUR_OUTLINE_COL);
+		setOutlineThickness(CUR_OUTLINE_SZ);
 	}
-	return selected;
+	
+	//TODO hardcoded data...
+	void Cursor::Update(Input action) {
+		switch (action)
+		{
+			case Input::LEFT:
+				if (mEntryIndex > 0) {
+					move({-CUR_MV_OFFSET,0});
+					mEntryIndex = std::clamp(--mEntryIndex, (size_t)0, (size_t)2);
+				}
+				break;
+			case Input::RIGHT:
+	
+				if (mEntryIndex < 2) {
+					move({CUR_MV_OFFSET,0});
+					mEntryIndex = std::clamp(++mEntryIndex, (size_t)0, (size_t)2);
+				}
+				break;
+			case Input::UP:
+				ChangeChar(Input::UP);
+				break;
+			case Input::DOWN:
+				ChangeChar(Input::DOWN);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	void Cursor::SetEntry(std::shared_ptr<sf::Text> &entry) { 
+		mEntryTxt = entry;
+		move(mEntryTxt->getGlobalBounds().position);
+	}
+	
+	//TODO refactor ranking system
+	bool Cursor::Accept(std::string* gameEntryName) {
+		if (gameEntryName != nullptr)
+			*gameEntryName = mEntryTxt->getString();
+		setOutlineColor(sf::Color::Transparent);
+		return true;
+	}
+	
+	void Cursor::ChangeChar(Input action) {
+		char selected = SelectChar(action);
+		std::string str = mEntryTxt->getString();
+		
+		str[mEntryIndex] = selected;
+		mEntryTxt->setString(str);
+	}
+	
+	char Cursor::SelectChar(Input action) {
+		static int index = -1;
+		char selected = 'A';
+		const int max = CHARSET_STR.size();
+	
+		switch (action)	{
+			case Input::UP:
+				index =  (index + 1) % max;
+				selected = CHARSET_STR[index];
+				break;
+			case Input::DOWN:
+				index = (index - 1 + max) % max;
+				selected = CHARSET_STR[index];
+				break;
+			default:
+				break;
+		}
+		return selected;
+	}
+
 }
