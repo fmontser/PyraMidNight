1c60a69eb97bdd2b4e86a9aadd26dbb53cb288a4
Lineath <fran.montserrat.developer@gmail.com>
2026-03-30 16:37:42 +0200

    Added resource manager class, audio pending

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -1,14 +1,42 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <memory>
 #include "Common.hpp"
 
 namespace fknd {
 	
 	class ResourceManager {
 		public:
-			ResourceManager();
+			static void Init();
+			static std::shared_ptr<sf::Texture> GetTexture(const std::string_view path);
+			static std::shared_ptr<sf::Font> GetFont(const std::string_view path);
+			
 		private:
+			struct Texture {
+				std::string_view             path;
+				std::shared_ptr<sf::Texture> texture;
+			};
+
+			struct Font {
+				std::string_view          path;
+				std::shared_ptr<sf::Font> font;
+			};
+
+			ResourceManager();
+			ResourceManager(const ResourceManager&) = delete;
+			ResourceManager& operator=(const ResourceManager&) = delete;
+
+			std::vector<Texture> mTextures;
+			std::vector<Font>    mFonts;
+
 			void ValidateLevels();
+			void LoadTextures();
+			void LoadFonts();
+
+			static ResourceManager& instance() {
+				static ResourceManager inst;
+				return inst;
+			}
 	};
 
 }
