dc370727d2ce01e3032ede5ba4c481f6739fa89f
Lineath <fran.montserrat.developer@gmail.com>
2026-04-18 19:44:31 +0200

    refactored Ball class to use new coldet system

--- src/GameObjects/Ball.cpp
+++ src/GameObjects/Ball.cpp
@@ -3,45 +3,71 @@
 #include "Ball.hpp"
 #include "AudioManager.hpp"
 #include "ResourceManager.hpp"
+#include "Obstacle.hpp"
+#include "Bumper.hpp"
+#include "Block.hpp"
 
 namespace pyramidnight {
 
 	Ball::Ball(const sf::Texture& texture) : sf::Sprite(texture) {
 		CollidableType = ICollidable::Type::BALL;
+		IsDynamic = true;
 		mSpeed = BALL_INIT_SPEED;
 		mDirection = BALL_INIT_DIR;
 		mRadius = texture.getSize().x / 2;
 		mDistance = 1.0f;
 		mState = State::DOCKED;
 		setOrigin({mRadius,mRadius});
-		ResetPos(BALL_INIT_POS);
+		Dock(BALL_INIT_POS);
 	}
 
-	void Ball::Launch() {
-		if (mState == State::DOCKED) {
+	Ball::State Ball::Update(UpdateBall update) {
+		switch (mState) {
+			case State::PLAYING: Move(update.deltaTime); CheckDeath(update.deathArea); break;
+			case State::DOCKED: Dock(update.bumperPos); Launch(update.action); break;
+			case State::DEAD: break;
+			default: break;
+		}
+		return mState;
+	}
+
+	ICollidable::Info Ball::OnCollision(ICollidable &collider) {
+		switch (collider.CollidableType) {
+			case ICollidable::Type::BLOCK: return OnBlockCollision(collider);
+			case ICollidable::Type::OBSTACLE: return OnObstacleCollision(collider);
+			case ICollidable::Type::BUMPER: return OnBumperCollision(collider);
+			default: break;
+		}
+		return { CollidableType, false, 0, std::nullopt };
+	}
+
+	void Ball::Move(const sf::Time& deltaTime) {
+		sf::Vector2f position = getPosition();
+		auto deltaTimeMs = deltaTime.asMilliseconds();
+
+		position.x += mDirection.x * mSpeed * deltaTimeMs;
+		position.y += mDirection.y * mSpeed * deltaTimeMs;
+		setPosition(position);
+	}
+
+	void Ball::CheckDeath(const sf::RectangleShape& deathArea) {
+		if (deathArea.getGlobalBounds().contains(getPosition()))
+			mState = State::DEAD;
+	}
+
+	void Ball::Launch(bool action) {
+		if (mState == State::DOCKED && action) {
 			mState = State::PLAYING;
 			auto sb = ResourceManager::GetAudio(PATH_AUD_BALL_LAUNCH);
 			AudioManager::Play({sb, VOL_AUD_BALL_LAUNCH, PolySound::Type::SFX, false});
 		}
 	}
 
-	void Ball::ResetPos(const sf::Vector2f &bumperPos) {
+	void Ball::Dock(const sf::Vector2f &bumperPos) {
 		auto newPos = bumperPos + BALL_DOCKED_OFFSET;
 		setPosition(newPos);
 		mDirection = BALL_INIT_DIR;
 		mState = State::DOCKED;
-	}
-
-	void Ball::Update(UpdateBall update) {
-		switch (mState) {
-			case State::PLAYING: Move(update.deltaTime); break;
-			case State::DOCKED:
-				ResetPos(update.bumperPos);
-				if (update.action)
-					Launch();
-				break;
-			default: break;
-		}
 	}
 
 	void Ball::Bounce(const sf::Sprite& obj) {
@@ -49,6 +75,7 @@ namespace pyramidnight {
 		auto rSize = obj.getGlobalBounds().size;
 		auto bPos = getPosition();
 		
+		//TODO fix overlap function
 		ResolveOverlap();
 		auto isBounceHorizontal = (bPos.x < rPos.x || bPos.x > (rPos.x + rSize.x));
 		auto isBounceVertical = (bPos.y < rPos.y || bPos.y > (rPos.y + rSize.y));
@@ -81,13 +108,33 @@ namespace pyramidnight {
 		AudioManager::Play({sb, VOL_AUD_BUMPER_BOUNCE, PolySound::Type::SFX, false});
 	}
 
-	ICollidable::Info Ball::OnCollision(ICollidable &collider) {
-		(void)collider;
-		return { CollidableType, false, 0, std::nullopt };
+	ICollidable::Info Ball::OnBlockCollision(ICollidable &collider) {
+		auto& block = static_cast<Block&>(collider);
+		auto cpos = GetCollisionPoint(block.getGlobalBounds());
+		if (cpos != std::nullopt)
+			Bounce(block);
+		return { CollidableType, false, 0, cpos };
 	}
 
-	std::optional<sf::Vector2f> Ball::GetCollisionPoint(const sf::FloatRect &rect)
-	{
+	ICollidable::Info Ball::OnObstacleCollision(ICollidable &collider) {
+		auto& obstacle = static_cast<Obstacle&>(collider);
+		auto cpos = GetCollisionPoint(obstacle.getGlobalBounds());
+		if (cpos != std::nullopt)
+			Bounce(obstacle);
+		return { CollidableType, false, 0, cpos };
+	}
+
+	ICollidable::Info Ball::OnBumperCollision(ICollidable &collider) {
+		auto& bumper = static_cast<Bumper&>(collider);
+		auto cpos = GetCollisionPoint(bumper.getGlobalBounds());
+		if (cpos != std::nullopt) {
+			Bounce(bumper);
+			ApplyBumperMod(bumper);
+		}
+		return { CollidableType, false, 0, cpos };
+	}
+
+	std::optional<sf::Vector2f> Ball::GetCollisionPoint(const sf::FloatRect &rect) {
 		sf::Vector2f closest {};
 		auto bPos = getPosition();
 		closest.x = std::clamp(bPos.x, rect.position.x, rect.position.x + rect.size.x);
@@ -101,18 +148,7 @@ namespace pyramidnight {
 		return std::nullopt;
 	}
 
-	const Ball::State &Ball::GetState() const { return mState; }
 
-	float Ball::GetRadius() const { return mRadius; }
-
-	void Ball::Move(const sf::Time& deltaTime) {
-		sf::Vector2f position = getPosition();
-		auto deltaTimeMs = deltaTime.asMilliseconds();
-
-		position.x += mDirection.x * mSpeed * deltaTimeMs;
-		position.y += mDirection.y * mSpeed * deltaTimeMs;
-		setPosition(position);
-	}
 
 	void Ball::ResolveOverlap() {
 		// bounce back from clipping
